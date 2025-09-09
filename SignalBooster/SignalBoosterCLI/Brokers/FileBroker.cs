4ce54ca34d092c4ded5614b7d8d9ef18031e1091
Nicholas Cicchetti <nc_cicchetti@yahoo.com>
2025-09-08 20:59:35 -0400

    Added FileValidationException

--- SignalBooster/SignalBoosterCLI/Brokers/FileBroker.cs
+++ SignalBooster/SignalBoosterCLI/Brokers/FileBroker.cs
@@ -1,15 +1,22 @@
 ﻿namespace SignalBoosterCLI.Brokers;
-
+using Exceptions;
 public class FileBroker : IFileBroker
 {
     public string ReadNote(string filePath)
     {
-        if (!File.Exists(filePath))
+        try
         {
-            return "Patient needs a CPAP with full face mask and humidifier. AHI > 20. Ordered by Dr. Cameron.";
-        }
+            if (!File.Exists(filePath))
+            {
+                return "Patient needs a CPAP with full face mask and humidifier. AHI > 20. Ordered by Dr. Cameron.";
+            }
 
-        return File.ReadAllText(filePath);
+            return File.ReadAllText(filePath);
+        }
+        catch (Exception exception)
+        {
+            throw new FileValidationException($"{filePath} had Validation errors see inner exception.",exception);
+        }
     }
 
 }
